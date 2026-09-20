f1acf66e1b4f3bd8e70500ab839386830d8ae38e
ryleespada <rspada@pratt.edu>
2026-09-20 01:57:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,3 +16,20 @@ photoInput.addEventListener("change", function () {
 
     findMatchButton.disabled = false;
 });
+
+findMatchButton.addEventListener("click", async function () {
+    const detectedLocation = document.getElementById("detectedLocation");
+
+    detectedLocation.textContent = "Contacting backend...";
+
+    try {
+        const response = await fetch("https://script.google.com/macros/s/AKfycbw4Bjtw9X2W2av06pTdpkuSWhd3EVdxGG-gFpm4-jJgpMxaIC2NRAbDAYnKzcl8-rXntg/exec");
+
+        const message = await response.text();
+
+        detectedLocation.textContent = message;
+    } catch (error) {
+        detectedLocation.textContent = "Could not contact backend.";
+        console.error(error);
+    }
+});
